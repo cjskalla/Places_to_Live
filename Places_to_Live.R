450163abef31e3406fe942cf544c3afdce33dcd4
cjskalla <calvin-skalla@uiowa.edu>
2020-07-26 18:39:42 -0500

    Keep cleaning code

--- Places_to_Live.R
+++ Places_to_Live.R
@@ -94,40 +94,13 @@ col_plot
 
 
 #Ratio Plot
-ratio_data <- mutate(full_data, "Quality.over.Cost" = 
-                       Quality.of.Life.Index/Cost.of.Living.Index, .before = Quality.of.Life.Index) %>% 
-  filter(Quality.of.Life.Index > 135)
-shrunk_country_data <- filter(full_data, Quality.of.Life.Index > 135)
-shrunk_country_data <- as.data.frame(shrunk_country_data[,1])
-ratio_data <- as.data.frame(ratio_data[,2])
-scaled_ratio_data <- as.data.frame(rescale.many(ratio_data, 1))
-ratio_binded_data <- cbind(shrunk_country_data, scaled_ratio_data)
-graph_ratio_data <- as.data.frame(ratio_binded_data[, c(1,3)])
-colnames(graph_ratio_data) = c("Country", "Ratio")
-graph_ratio_data <- graph_ratio_data %>% arrange(desc(Ratio))
-
-
-
-ratio_plot <- ggplot(graph_ratio_data[1:10,], aes(y = reorder(Country, Ratio), 
-                                 x = Ratio)) +
-  geom_point(color = "blue", size = 5,
-             fill = alpha("lightblue", 0.5), shape = 21, stroke = 2)  +
-  geom_segment(aes(x = 0, xend = Ratio, y = Country, yend = Country), color = "white") + 
-  labs(title = "Top 10 Quality over Cost", 
-       x = "Quality over Cost Percentile", y = "Country") + 
-  theme_solarized_2(light=FALSE) + scale_colour_solarized('blue') +
-  theme(axis.text.y = element_text(angle = 360))
-
-
-ratio_plot
-
-
-#Ratio Plot 2.0
-ratio_2_data <- scaled_full_data %>% arrange(desc(Quality.Cost.rescaled))
-ratio_2_plot <- ggplot(ratio_2_data[1:10,], aes(y = reorder(Country, Quality.Cost.rescaled), 
+ratio_data <- scaled_full_data %>% arrange(desc(Quality.Cost.rescaled))
+ratio_plot <- ggplot(ratio_data[1:10,], aes(y = reorder(Country, Quality.Cost.rescaled), 
                                                   x = Quality.Cost.rescaled)) +
-  geom_point(color = "blue", size = 5,
-             fill = alpha("lightblue", 0.5), shape = 21, stroke = 2)  +
+  geom_point(aes(y = reorder(Country, Quality.Cost.rescaled), 
+                                             x = Quality.Cost.rescaled, 
+                                             size = WellBeing.rescaled*10), 
+             color = "blue", fill = alpha("lightblue", 0.5), shape = 21, stroke = 2)  +
   geom_segment(aes(x = 0, xend = Quality.Cost.rescaled, y = Country, yend = Country), color = "white") + 
   labs(title = "Top 10 Quality over Cost", 
        x = "Quality over Cost Percentile", y = "Country") + 
@@ -135,7 +108,7 @@ ratio_2_plot <- ggplot(ratio_2_data[1:10,], aes(y = reorder(Country, Quality.Cos
   theme(axis.text.y = element_text(angle = 360))
 
 
-ratio_2_plot
+ratio_plot
 
 
 #Property Price to Income Graph
@@ -150,38 +123,13 @@ ppi_plot <- ggplot(ppi_data[1:10,]) +
 ppi_plot
 
 
-#Calculate a personal well-being rating off of col(6:9)
-wellbeing_data <- full_data[,c(6:9)]
-country_data <- as.data.frame(full_data[,1])
 
-scaled_wellbeing_data <- as.data.frame(rescale.many(wellbeing_data, c(1,2,3,4)))
-scaled_wellbeing_data <- scaled_wellbeing_data[, c(5:8)]
-
-binded_data <- cbind(country_data, scaled_wellbeing_data)
-colnames(binded_data) = c("Country", "Health", "Pollution", "Climate",
-                          "Safety")
-equation_data <- mutate(scaled_full_data, "wellbeing" = Health.Care - Pollution + 
-                          Climate + Safety)  %>% arrange(desc(wellbeing))
 
 #Graph for wellbeing data
-wb_data <- as.data.frame(equation_data[,6])
-scaled_wb_data <- as.data.frame(rescale.many(wb_data, 1))
-wb_binded_data <- cbind(country_data, scaled_wb_data)
-graph_wb_data <- as.data.frame(wb_binded_data[, c(1,3)])
-colnames(graph_wb_data) = c("Country", "wellbeing")
-
-
-wellbeing_plot <- ggplot(equation_data[1:10,], aes(x = wellbeing, y = reorder(Country, wellbeing))) +
-  geom_point(color = "blue", size = 2, 
-             fill = alpha("lightblue", 0.5), shape = 21, stroke = 2)  +
-  geom_segment(aes(x = 0, xend = wellbeing, y = Country, yend = Country)) + 
-  labs(title = "Top 10 Well-being", 
-       x = "Total Well-being", y = "Country") + 
-  theme_wsj()+ scale_colour_wsj("colors6")
-
-wellbeing_plot <- ggplot(graph_wb_data[1:10,], aes(x = wellbeing, y = reorder(Country, wellbeing))) +
-  geom_col(aes(y = wellbeing, 
-               x = reorder(Country, wellbeing)), fill = "#800000", width = 0.5)  + 
+well_being_data <- scaled_full_data %>% arrange(desc(WellBeing.rescaled))
+wellbeing_plot <- ggplot(well_being_data[1:10,], aes(x = WellBeing.rescaled, y = reorder(Country, WellBeing.rescaled))) +
+  geom_col(aes(y = WellBeing.rescaled, 
+               x = reorder(Country, WellBeing.rescaled)), fill = "#800000", width = 0.5)  + 
   theme_wsj()+ scale_colour_wsj("colors6") + coord_flip() +
   labs(title = "Top 10 Well-being", 
        x = "Total Well-being", y = "Country", caption = 
@@ -189,6 +137,4 @@ wellbeing_plot <- ggplot(graph_wb_data[1:10,], aes(x = wellbeing, y = reorder(Co
        and Safety")
 
 wellbeing_plot
-
-
 
